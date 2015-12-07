90fdeebb4eb3c9fc9840bb9bb33393abe5091e8d
aboc <abocd@qq.com>
2015-12-07 13:43:12 +0800

    add help

--- cron.go
+++ cron.go
@@ -10,11 +10,13 @@ import (
 	"net/http"
 
 	"os"
-	"time"
 	"strconv"
+	"time"
 )
+
 //请求总次数
 var total int
+
 //是否输出结果
 var result bool
 
@@ -22,8 +24,7 @@ func request_cron(url string) {
 
 	total++
 
-
-	fmt.Println("Request num :",total)
+	fmt.Println("Request num :", total)
 
 	resp, err1 := http.Get(url)
 
@@ -46,9 +47,9 @@ func request_cron(url string) {
 		return
 
 	}
-	
+
 	if result {
-	fmt.Print(string(data))	
+		fmt.Print(string(data))
 	}
 	return
 
@@ -59,11 +60,25 @@ func request_cron(url string) {
 func main() {
 	param := os.Args
 	data := make(map[string]string)
-	len := len(param) - 1
-	// fmt.Println(len)
+	len := len(param)
 	for i := 1; i < len; i += 2 {
 		// fmt.Println(param[i])
-		data[param[i]] = param[i+1]
+		if (i+1) < len && param[i+1] != "" {
+			data[param[i]] = param[i+1]
+		} else {
+			data[param[i]] = ""
+		}
+	}
+	// fmt.Println(data)
+	_, ok := data["-help"]
+	_, ok2 := data["-h"]
+	if ok || ok2 {
+		fmt.Println("cron help!")
+		fmt.Println("-url        :请求的url地址")
+		fmt.Println("-t            :每次请求的间隔秒  5")
+		fmt.Println("-n            :每次请求多少次   1")
+		fmt.Println("-r            :是否输出结果   true")
+		return
 	}
 	//default
 	//请求的url地址
@@ -71,19 +86,19 @@ func main() {
 		fmt.Println("Url is empty!")
 		return
 	}
-	//每次请求的间隔秒杀
+	//每次请求的间隔秒
 	if data["-t"] == "" {
-            data["-t"] = "5"
+		data["-t"] = "5"
 	}
 	//每次请求多少次
 	if data["-n"] == "" {
-            data["-n"] = "1"
+		data["-n"] = "1"
 	}
 	//是否输出结果
 	if data["-r"] == "" {
-            data["-r"] = "true"
+		data["-r"] = "true"
 	}
-	if data["-r"]== "true"{
+	if data["-r"] == "true" {
 		result = true
 	} else {
 		result = false
@@ -91,20 +106,20 @@ func main() {
 	//fmt.Println(data["bbb"])
 	total = 0
 	fmt.Println("Start..")
-//转化为float64位
-	second_h,_ := strconv.ParseFloat(data["-t"],64)
-//转化为微秒
-	second_m := second_h*1e9
-//转化为符合time.Sleep的参数
+	//转化为float64位
+	second_h, _ := strconv.ParseFloat(data["-t"], 64)
+	//转化为微秒
+	second_m := second_h * 1e9
+	//转化为符合time.Sleep的参数
 	second_num := time.Duration(second_m)
-        n,_ := strconv.ParseInt(data["-n"],10,64)
+	n, _ := strconv.ParseInt(data["-n"], 10, 64)
 	var i int64
 	for {
-		fmt.Println("Next..")		
-		for i=0;i<n;i++{
-     		    go request_cron(data["-url"])
+		fmt.Println("Next..")
+		for i = 0; i < n; i++ {
+			go request_cron(data["-url"])
 		}
-		
+
 		time.Sleep(second_num)
 
 	}
